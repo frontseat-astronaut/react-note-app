e3add0866cea73e5264b7f1528a6851cdd9da494
mathur.shubhad <mathur.shubhad@uber.com>
2021-08-09 15:27:03 +0530

    added smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import './App.css';
 import React from 'react';
+var Scroll   = require('react-scroll');
+var ScrollElement  = Scroll.Element;
+var scroller = Scroll.scroller;
 
 let defaultNotes = [
-  "A note is a string of text placed at the bottom of a page in a book or document or at the end of a chapter, volume or the whole text. The note can provide an author's comments on the main text or citations of a reference work in support of the text.\nFootnotes are notes at the foot of the page while endnotes are collected under a separate heading at the end of a chapter, volume, or entire work. Unlike footnotes, endnotes have the advantage of not affecting the layout of the main text, but may cause inconvenience to readers who have to move back and forth between the main text and the endnotes.\nIn some editions of the Bible, notes are placed in a narrow column in the middle of each page between two columns of biblical text.",
+  "Note #1\nA note is a string of text placed at the bottom of a page in a book or document or at the end of a chapter, volume or the whole text. The note can provide an author's comments on the main text or citations of a reference work in support of the text.\nFootnotes are notes at the foot of the page while endnotes are collected under a separate heading at the end of a chapter, volume, or entire work. Unlike footnotes, endnotes have the advantage of not affecting the layout of the main text, but may cause inconvenience to readers who have to move back and forth between the main text and the endnotes.\nIn some editions of the Bible, notes are placed in a narrow column in the middle of each page between two columns of biblical text.",
   "Note #2",
   "Note #3",
   "Note #4",
-  "Note #5",
-  "Note #6",
-  "Note #7",
 ];
 
 class Note extends React.Component {
   constructor(props) {
     super(props);
 
-    this.key = props.key;
     this.deleteNote = props.deleteNote;
     this.parentRefresh = props.parentRefresh;
 
-    this.state = { "text": props.text, "newText": "", "isTyping": false };
+    this.state = { "text": props.text, "newText": props.text, "isTyping": (props.justCreated) ? true : false, "justCreated": props.justCreated };
 
     this.editNote = this.editNote.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+
+  }
+
+  componentDidMount(){
+    if(this.state.justCreated)
+      this.parentRefresh();
   }
 
   editNote(event) {
@@ -38,11 +43,15 @@ class Note extends React.Component {
   }
 
   handleSubmit(event) {
-    this.setState({ "isTyping": false, "text": this.state.newText });
+    this.setState({ "isTyping": false, "text": this.state.newText, "justCreated": false });
     this.parentRefresh();
   }
 
   handleCancel(event) {
+    if (this.state.justCreated) {
+      this.deleteNote(null, true);
+      return;
+    }
     this.setState({ "isTyping": false });
     this.parentRefresh();
   }
@@ -54,7 +63,7 @@ class Note extends React.Component {
           (!this.state.isTyping) ? (
             <div className="Options">
               <a href="#" onClick={this.deleteNote}>[x]</a>
-              <br/>
+              <br />
               <a href="#" onClick={this.editNote}>[/]</a>
             </div>
           ) : (null)
@@ -62,7 +71,7 @@ class Note extends React.Component {
         {
           (this.state.isTyping) ? (
             <div>
-              <textarea className="NoteText" value={this.state.newText} readOnly={false} onChange={this.handleChange} /> <br />
+              <textarea autoFocus className="NoteText" value={this.state.newText} readOnly={false} onChange={this.handleChange} /> <br />
               <button className="DoneButton" onClick={this.handleSubmit}> Done </button>
               <button className="CancelButton" onClick={this.handleCancel}> Cancel </button>
             </div>
@@ -79,6 +88,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.createNote = this.createNote.bind(this);
     this.displayNotes = this.displayNotes.bind(this);
     this.newNoteButton = this.newNoteButton.bind(this);
     this.handleNewNote = this.handleNewNote.bind(this);
@@ -87,13 +97,25 @@ class App extends React.Component {
 
     let jsxNotes = {}
     for (let i = 0; i < defaultNotes.length; ++i) {
-      jsxNotes[i] = <Note text={defaultNotes[i]} key={i} deleteNote={this.deleteNote(i)} parentRefresh={this.refresh} />;
+      jsxNotes[i] = this.createNote(i, defaultNotes[i], false);
     }
-    this.state = { "notes": jsxNotes, "editMode": false, "totalNotesCounter": jsxNotes.length };
+    this.state = { "notes": jsxNotes, "editMode": false, "totalNotesCounter": Object.keys(jsxNotes).length };
   }
 
-  refresh() {
-    this.setState({ "dummyParam": false });
+  createNote(key, initText, justCreated) {
+    return (
+      <div key={key}>
+      <ScrollElement name={`Note ${key}`}></ScrollElement>
+      <Note text={initText} deleteNote={this.deleteNote(key)} parentRefresh={this.refresh(key)} justCreated={justCreated} />
+      </div>);
+  }
+
+  refresh(key) {
+    let parentRefresh = function() {
+      this.setState({});
+      scroller.scrollTo(`Note ${key}`, {'smooth':true, 'duration': 700});
+    };
+    return parentRefresh.bind(this);
   }
 
   displayNotes() {
@@ -113,14 +135,15 @@ class App extends React.Component {
   }
 
   handleNewNote(event) {
-    // let newNote = <Note
-    // this.setState({"editMode": true})
+    let newNote = this.createNote(this.state.totalNotesCounter, "", true);
+    let newNotes = this.state.notes;
+    newNotes[this.state.totalNotesCounter] = newNote;
+    this.setState({ "editMode": true, "totalNotesCounter": this.state.totalNotesCounter + 1 });
   }
 
   deleteNote(key) {
-    let handleDelete = function (event) {
-      if(window.confirm(`Delete Note #${key+1}?`))
-      {
+    let handleDelete = function (event, cancelled=false) {
+      if (cancelled || window.confirm(`Delete Note?`)) {
         let notes = this.state.notes;
         delete notes[key];
         this.setState({ "notes": notes });
